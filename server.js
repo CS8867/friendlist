2bc210c5800b029715644fcde5e80bee7e015e31
Chaitanya Subhedar <chaitanyasubhedar@gmail.com>
2021-09-18 12:59:01 +0530

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const { json } = require('express');
-const express = require('express');
-const connectDB = require('./config/db');
+const { json } = require("express");
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -8,16 +9,26 @@ const app = express();
 connectDB();
 
 // Init middleware
-app.use(express.json({extended: false}))
+app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to the Contacts Keeper API' })
-})
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to the Contacts Keeper API" });
+});
 
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "path", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
