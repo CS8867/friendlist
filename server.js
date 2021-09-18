3c8800aff7501192f9d19b4a6e75067e09e09f0e
Chaitanya Subhedar <chaitanyasubhedar@gmail.com>
2021-09-18 13:15:37 +0530

    Removed initial route

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ connectDB();
 // Init middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to the Contacts Keeper API" });
-});
-
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
